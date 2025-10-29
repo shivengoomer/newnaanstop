0c467dfb940bd12fcd7b972e5ef6a08622169824
Shiven Goomer <shivengoomer@gmail.com>
2025-10-29 23:21:33 +0530

    fixed navbar

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -3,8 +3,6 @@ import { HeaderItem } from "@/types/menu";
 export const headerData: HeaderItem[] = [
   { label: "Home", href: "/#home-section" },
   { label: "About Us", href: "/#about-section" },
-  { label: "Recipe", href: "/#cook-section" },
-  {label:"Our Collections",href:"/recipe"},
+  { label: "Recipes", href: "/recipe" },
   { label: "Gallery", href: "/#gallery-section" },
-
 ];
